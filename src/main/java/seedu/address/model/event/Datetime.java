d2496488b0ed46d01758bdb34f928a4d4e4fc170
Kai Yu Quek <kaiyu92@hotmail.com>
2017-10-26 20:57:54 +0800

    no message

--- src/main/java/seedu/address/model/event/Datetime.java
+++ src/main/java/seedu/address/model/event/Datetime.java
@@ -17,7 +17,7 @@ public class Datetime {
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String DATETIME_VALIDATION_REGEX =
-            "^((0[1-9])|([012][0-9])|(3[01]))-(0[1-9]|1[012])-\\d\\d\\d\\d (([0-1][0-9])|(2[0-3]))[0-5][0-9]\\s?$";
+            "^((0[1-9])|([012][0-9])|(3[01]))-(0[1-9]|1[012])-\\d\\d\\d\\d (([0-1][0-9])|(2[0-3]))[0-5][0-9][\\s]?";
 
     public final String value;
 
